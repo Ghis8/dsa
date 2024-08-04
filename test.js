d3401ee3e8621388d336ab815a8fc414c3d4d0a2
ghis8 <ghislainkongolo@yahoo.com>
2024-08-04 14:19:16 +0200

    added new data structure studies

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-let  myObj={}
-if(myObj){
-    console.log('true')
+let names=["ghislain","angelique","astrid","josline"]
+
+for( let i=0;i<names.length;i++){
+    names[i]=names[i+1]
 }
-else{
-    console.log('false')
-}
+names.splice(names.length-1,1)
+
+
+console.log(names)
