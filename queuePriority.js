246a9a053d881c3c8af19c00fef93ef57a3ad394
ghis8 <ghislainkongolo@yahoo.com>
2024-08-08 23:46:26 +0200

    new changes added

--- queuePriority.js
+++ queuePriority.js
@@ -1,10 +1,30 @@
-let items=new WeakMap()
-
-class Queue{
+class PriorityQueue{
     constructor(){
-        items.set(this,[])
+        this.items=[]
     }
-    queueElement(element, priority){
-        
+    enqueue(element,priority){
+        let added=false
+
+        for(let i=0;i<this.items.length ; i++){
+            if(priority < this.items[i].priority){
+                this.items.splice(i,0,{element,priority})
+                added=true
+                return
+            }
+        }
+        if(!added){
+            this.items.push({element,priority})
+        }
+    }
+    toString(){
+        return this.items
     }
 }
+
+const queue=new PriorityQueue()
+queue.enqueue("ghis",1)
+queue.enqueue("astrid",3)
+queue.enqueue("josline",4)
+queue.enqueue("angel",2)
+
+console.log(queue.toString())
