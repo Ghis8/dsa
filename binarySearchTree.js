7f3235ae850c77a3e400d47b7f0f25fe58c17248
ghis8 <ghislainkongolo@yahoo.com>
2024-08-12 03:51:24 +0200

    working on BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,4 +10,28 @@ class BinarySearchTree{
     constructor(){
         this.root=null
     }
+    insert(key){
+        let newNode=new Node(key)
+        if(this.root == null){
+            this.root=newNode
+        }else{
+            this.insertNode(root,newNode)
+        }
+    }
+    insertNode(node, newNode){
+        // only insert the lower key on the left side and the higher on the right side of the tree
+        if(newNode.key < node.key){
+            if(node.left == null){
+                node.left=newNode
+            }else{
+                this.insertNode(node.left,newNode)
+            }
+        }else{
+            if(node.right ===null){
+                node.right=newNode
+            }else{
+                this.insertNode(node.right,newNode)
+            }
+        }
+    }
 }
